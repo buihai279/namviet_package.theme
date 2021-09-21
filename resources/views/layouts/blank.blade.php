c80d1f53e496b6b7325556d2048f8766b2308e1e
haibv <haibv@namviet-corp.com.vn>
2021-09-21 10:20:11 -0700

    update theme blank

---
+++ resources/views/layouts/blank.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts::base')
+@push('head')
+    <!--begin::Fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet">
+    <!--end::Fonts-->
+    <!--begin::Page Vendor Stylesheets(used by this page)-->
+    @stack('style_head')
+    <!--end::Page Vendor Stylesheets-->
+    <!--begin::Global Stylesheets Bundle(used by all pages)-->
+    <link href="{{asset('vendor/theme/js/plugins/global/plugins.bundle.css')}}" rel="stylesheet"
+          type="text/css"/>
+    <link href="{{asset('vendor/theme/css/style.bundle.css')}}" rel="stylesheet" type="text/css"/>
+    <!--end::Global Stylesheets Bundle-->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <script src="{{asset('vendor/theme/js/libs/axios.min.js')}}"></script>
+    <script src="{{asset('vendor/theme/js/libs/lodash.min.js')}}"></script>
+    <script src="{{asset('vendor/theme/js/libs/moment/moment.min.js')}}"></script>
+@endpush
+@section('body')
+    <!--begin::Body-->
+    <body id="kt_body"
+          class="header-fixed header-tablet-and-mobile-fixed toolbar-enabled toolbar-fixed aside-enabled aside-fixed"
+          style="--kt-toolbar-height:55px;--kt-toolbar-height-tablet-and-mobile:55px">
+    <!--begin::Main-->
+    <!--begin::Root-->
+    <div class="d-flex flex-column flex-root">
+        <!--begin::Page-->
+        <div class="page d-flex flex-row flex-column-fluid">
+            <!--begin::Aside-->
+            <!--end::Aside-->
+            <!--begin::Wrapper-->
+            <div class="wrapper d-flex flex-column flex-row-fluid" id="kt_wrapper">
+                <!--begin::Header-->
+                <!--end::Header-->
+                <!--begin::Content-->
+            @yield('content')
+            <!--end::Content-->
+                <!--begin::Footer-->
+                <!--end::Footer-->
+            </div>
+            <!--end::Wrapper-->
+        </div>
+        <!--end::Page-->
+    </div>
+    {{--    @include('layouts::_scroll_top')--}}
+    <!--end::Main-->
+    <!--begin::Javascript-->
+    <!--begin::Global Javascript Bundle(used by all pages)-->
+    <script src="{{asset('vendor/theme/js/plugins/global/plugins.bundle.js')}}"></script>
+    <script src="{{asset('vendor/theme/js/scripts.bundle.js')}}"></script>
+
+    <!--end::Global Javascript Bundle-->
+    <!--begin::Page Custom Javascript(used by this page)-->
+    @stack('script_bottom')
+    <!--end::Page Custom Javascript-->
+    <!--end::Javascript-->
+    </body>
+    <!--end::Body-->
+@endsection
---
+++ resources/views/layouts/components_v8/alerts.blade.php
@@ -0,0 +1,49 @@
+@php
+    $errorMsg=[];
+    if (!empty(request()->session()->get('error'))){
+        $errorMsg[]=request()->session()->get('error');
+    }
+    if(!empty($errors->all())){
+        $errorMsg=array_merge($errorMsg,$errors->all());
+    }
+@endphp
+@if(!empty($errorMsg))
+    <!--begin::Alert-->
+    <div class="alert alert-dismissible bg-light-danger d-flex flex-column flex-sm-row p-5 mb-10">
+        <!--begin::Icon-->
+        <span class=" me-4 mb-5 mb-sm-0">
+        <i class="la la-comment la-3x"></i>
+    </span>
+        <!--end::Icon-->
+
+        <!--begin::Wrapper-->
+        <div class="d-flex flex-column pe-0 pe-sm-10">
+            <!--begin::Title-->
+            <h4 class="fw-bold">Có lỗi xảy ra</h4>
+            <!--end::Title-->
+            <!--begin::Content-->
+            <span>
+
+            <ul>
+                @foreach ($errorMsg as $msg)
+                    <li>{{ $msg }}</li>
+                @endforeach
+            </ul>
+            </span>
+            <!--end::Content-->
+        </div>
+        <!--end::Wrapper-->
+
+        <!--begin::Close-->
+        <button type="button"
+                class="position-absolute position-sm-relative m-2 m-sm-0 top-0 end-0 btn btn-icon ms-sm-auto"
+                data-bs-dismiss="alert">
+        <span class="svg-icon svg-icon-1 svg-icon-danger">
+
+        <i class="la la-close la-3x"></i>
+        </span>
+        </button>
+        <!--end::Close-->
+    </div>
+    <!--end::Alert-->
+@endif
